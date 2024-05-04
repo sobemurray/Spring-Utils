f5bd5091b68a689c59a0baf2dcf3763325e9fe9c
John Murray <john.murray@sobetech.com>
2024-05-04 10:14:17 -0400

    SU-5 Added simple method to write a file

--- src/main/java/com/sobetech/common/spring/service/io/FileUtil.java
+++ src/main/java/com/sobetech/common/spring/service/io/FileUtil.java
@@ -11,9 +11,15 @@
  */
 package com.sobetech.common.spring.service.io;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -28,6 +34,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class FileUtil
 {
+	protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	
 	/**
 	 * Gets the file path string for the resources directory of a codebase
 	 * 
@@ -48,5 +56,25 @@ public class FileUtil
 	{
 		Path resourceDirectory = Paths.get("src","test","resources");
 		return resourceDirectory.toFile().getAbsolutePath();
+	}
+	
+	public boolean savePlayersFile(String filePath, Collection<Object> objectsToWriteToFile)
+	{
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) 
+        {
+            // Iterate over the list and write each object's data fields separated by pipes
+            for (Object objectsToWrite : objectsToWriteToFile) 
+            {
+                writer.write(objectsToWrite.toString());
+                writer.newLine(); // Move to the next line
+            }
+        } 
+        catch (IOException e) 
+        {
+            LOG.error("File could not be saved", e);
+            return false;
+        }
+        
+        return true;
 	}
 } 
